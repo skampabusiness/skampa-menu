88155bf3f9c7a28482ea9df72dc4b133b1afce46
skampabusiness <skampabusiness@gmail.com>
2024-11-01 11:07:52 -0400

    Update base URL for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/skampa-menu/',
+  base: '/', // Changed from '/skampa-menu/' to '/'
   define: {
     'process.env': {}
   }
